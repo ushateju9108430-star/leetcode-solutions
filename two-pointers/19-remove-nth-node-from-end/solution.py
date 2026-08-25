51d913daf7fe95e9f09e3523a20d21f77a11fe3b
Usha <ushateju9108430@gmail.com>
2026-08-25 20:35:09 +0530

    Implement removeNthFromEnd method in Solution class

---
+++ two-pointers/19-remove-nth-node-from-end/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+
+        slow = dummy
+        fast = dummy
+
+        for i in range(n):
+            fast = fast.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
